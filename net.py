0fabacdf2b12fd9b5e0120272a6fff618f2f7558
ZhaoYiming <61368442+ZYM-PKU@users.noreply.github.com>
2020-05-17 21:49:25 +0800

    Update net.py

--- net.py
+++ net.py
@@ -1,58 +1,68 @@
 import torch
 import torch.nn as nn
-from function import AdaIN
 import torchvision.models as models
+from function import AdaIN,get_mean_std
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")#训练设备
 
 
-##############定义前传网络#############
-class FPnet(nn.Module):
-    def __init__(self,decoder):
-        super(FPnet, self).__init__()
-        self.net = models.vgg19(pretrained=True).features.eval()#使用预训练完成的vgg16
-        self.mseloss = nn.MSELoss()
-        self.decode=decoder
-        for param in self.net.parameters():
-            param.requires_grad = False#对encoder不进行梯度下降
 
-    def encode(self, x,layer=0):#基于vgg19编码提取特定层的特征图
-        '''分别提取relu1-1,relu2-1,relu3-1,relu4-1的特征，默认提取relu4-1'''
-        layers=[21,2,7,12,21]
-        for i in range(layers[layer]):
-            x = self.net[i](x)
-        return x
-    def content_loss(self,feature1,feature2):
-        '''计算内容损失'''
-        dis=self.mseloss(feature1,feature2)
-        return dis
-    def style_loss(self,img1,img2):
-        '''计算样式损失'''
-        #提取四个relu层输出
-        features1=list(self.encode(img1,layer=l)for l in range(1,5))
-        features2=list(self.encode(img2,layer=l)for l in range(1,5))
-        #求取均值与标准差
-        mean=torch.Tensor(list(self.mseloss(features1[i].mean(),features2[i].mean())  for i in range(4))).sum()
-        std=torch.Tensor(list(self.mseloss(features1[i].std(),features2[i].std())  for i in range(4))).sum()
-        dis=mean+std
-        return dis
-
-
-    def forward(self,content,style,alpha=1.0,lamda=10.0,require_loss=True):
-        '''一次前传计算损失'''
-        content=content.to(device)
-        style=style.to(device)
-        style_features=self.encode(style)
-        content_features=self.encode(content)
-
-        t=AdaIN(content_features,style_features)
-        t=alpha*t+(1-alpha)*content_features
-
-        output=self.decode(t)
-        if not require_loss:return output
-
-        out_features=self.encode(output)
-        return self.content_loss(out_features,t)+self.style_loss(output,style)*lamda,output
+#############vgg编码器##############
+vgg = nn.Sequential(
+    nn.Conv2d(3, 3, (1, 1)),
+    nn.ReflectionPad2d((1, 1, 1, 1)),
+    nn.Conv2d(3, 64, (3, 3)),
+    nn.ReLU(),  # relu1-1
+    nn.ReflectionPad2d((1, 1, 1, 1)),
+    nn.Conv2d(64, 64, (3, 3)),
+    nn.ReLU(),  # relu1-2
+    nn.MaxPool2d((2, 2), (2, 2), (0, 0), ceil_mode=True),
+    nn.ReflectionPad2d((1, 1, 1, 1)),
+    nn.Conv2d(64, 128, (3, 3)),
+    nn.ReLU(),  # relu2-1
+    nn.ReflectionPad2d((1, 1, 1, 1)),
+    nn.Conv2d(128, 128, (3, 3)),
+    nn.ReLU(),  # relu2-2
+    nn.MaxPool2d((2, 2), (2, 2), (0, 0), ceil_mode=True),
+    nn.ReflectionPad2d((1, 1, 1, 1)),
+    nn.Conv2d(128, 256, (3, 3)),
+    nn.ReLU(),  # relu3-1
+    nn.ReflectionPad2d((1, 1, 1, 1)),
+    nn.Conv2d(256, 256, (3, 3)),
+    nn.ReLU(),  # relu3-2
+    nn.ReflectionPad2d((1, 1, 1, 1)),
+    nn.Conv2d(256, 256, (3, 3)),
+    nn.ReLU(),  # relu3-3
+    nn.ReflectionPad2d((1, 1, 1, 1)),
+    nn.Conv2d(256, 256, (3, 3)),
+    nn.ReLU(),  # relu3-4
+    nn.MaxPool2d((2, 2), (2, 2), (0, 0), ceil_mode=True),
+    nn.ReflectionPad2d((1, 1, 1, 1)),
+    nn.Conv2d(256, 512, (3, 3)),
+    nn.ReLU(),  # relu4-1, this is the last layer used
+    nn.ReflectionPad2d((1, 1, 1, 1)),
+    nn.Conv2d(512, 512, (3, 3)),
+    nn.ReLU(),  # relu4-2
+    nn.ReflectionPad2d((1, 1, 1, 1)),
+    nn.Conv2d(512, 512, (3, 3)),
+    nn.ReLU(),  # relu4-3
+    nn.ReflectionPad2d((1, 1, 1, 1)),
+    nn.Conv2d(512, 512, (3, 3)),
+    nn.ReLU(),  # relu4-4
+    nn.MaxPool2d((2, 2), (2, 2), (0, 0), ceil_mode=True),
+    nn.ReflectionPad2d((1, 1, 1, 1)),
+    nn.Conv2d(512, 512, (3, 3)),
+    nn.ReLU(),  # relu5-1
+    nn.ReflectionPad2d((1, 1, 1, 1)),
+    nn.Conv2d(512, 512, (3, 3)),
+    nn.ReLU(),  # relu5-2
+    nn.ReflectionPad2d((1, 1, 1, 1)),
+    nn.Conv2d(512, 512, (3, 3)),
+    nn.ReLU(),  # relu5-3
+    nn.ReflectionPad2d((1, 1, 1, 1)),
+    nn.Conv2d(512, 512, (3, 3)),
+    nn.ReLU()  # relu5-4
+)
 
 
 ############定义镜像解码器#############
@@ -95,7 +105,6 @@ class Decoder(nn.Module):
         self.pre42=nn.ReflectionPad2d((1, 1, 1, 1))
         self.conv42=nn.Conv2d(in_channels=64,out_channels=3,kernel_size=3,stride=1)
         self.relu42=nn.ReLU(inplace=True)
-        self.softmax=nn.Softmax2d()
 
     def forward(self, x):
         x=self.pre11(x)
@@ -120,4 +129,55 @@ class Decoder(nn.Module):
         x=self.pre42(x)
         x=self.conv42(x)
         return x
- 
+
+##############定义前传网络#############
+class FPnet(nn.Module):
+    def __init__(self,decoder):
+        super(FPnet, self).__init__()
+        self.encoder = vgg
+        self.decoder = decoder
+        self.mseloss = nn.MSELoss()
+        self.encoder.load_state_dict(torch.load('model/vgg_normalised.pth'))
+        for param in self.encoder.parameters():
+            param.requires_grad = False#对encoder不进行梯度下降
+
+    def encode(self, x,layer=0):#基于vgg19编码提取特定层的特征图
+        '''分别提取relu1-1,relu2-1,relu3-1,relu4-1的特征，默认提取relu4-1'''
+        layers=[31,4,11,18,31]
+        for i in range(layers[layer]):
+            x = self.encoder[i](x)
+        return x
+
+    def get_content_loss(self,feature1,feature2):
+        '''计算内容损失'''
+        dis=self.mseloss(feature1,feature2)
+        return dis
+
+    def get_style_loss(self, input, target):
+        '''计算样式损失'''
+        input_mean, input_std = get_mean_std(input)
+        target_mean, target_std = get_mean_std(target)
+        return self.mseloss(input_mean, target_mean) + self.mseloss(input_std, target_std)
+
+    def forward(self,content,style,alpha=1.0,lamda=10.0,require_loss=True):
+        '''一次前传计算损失'''
+        content=content.to(device)
+        style=style.to(device)
+
+        style_features=self.encode(style)
+        content_features=self.encode(content)
+
+        t=AdaIN(content_features,style_features)
+        t=alpha*t+(1-alpha)*content_features
+
+        output=self.decoder(t)
+        if not require_loss:return output
+
+        out_features=self.encode(output)
+        content_loss=self.get_content_loss(out_features,t)
+        style_loss=self.get_style_loss(out_features,style_features)
+
+        for i in range(1,4):
+            style_loss+=self.get_style_loss(self.encode(output,layer=i),self.encode(style,layer=i))
+
+        return content_loss + style_loss*lamda , output
